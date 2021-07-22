48798151b61012d6652aa02b67e06f0e9b91978a
Shunyakatada <85655151+shunyasaitama@users.noreply.github.com>
2021-07-22 19:02:35 +0900

    users_viewの作成とusers_routeの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,3 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/new'
-  get 'users/create'
-  get 'users/show'
-  get 'users/edit'
-  get 'users/update'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
 end
